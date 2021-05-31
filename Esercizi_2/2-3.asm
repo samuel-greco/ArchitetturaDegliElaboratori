afe7886c926c687465229c1e46f7e8d9ec6e6018
samuel-greco <greco.1958358@studenti.uniroma1.it>
2021-05-31 12:57:17 +0200

    Aggiunta esercizio 2/3

--- Esercizi_2/2-3.asm
+++ Esercizi_2/2-3.asm
@@ -4,33 +4,57 @@
 # INPUT: 521 (in binario 1000001001)
 # OUTPUT:3
 
-.eqv ITER, $t9
-.eqv DIM, $t8
+# 11111111111111111111111111111110 = > 4294967294
+# 11111111111111111111111111111111 = > 4294967295
+
 .text
 .globl main
 main:
-	la $a0, str_input
+#	Definisci $t9 a 0 come sommatore
+	li $t9, 0
+#	Definisci $t8 a 2 come divisore
+	li $t8, 2
+
+#	Stampa la stringa di input
+	la $a0, str_input	
+	li $v0, 4
+	syscall
+
+#	Acquisisci numero in input
+	li $v0, 5
+	syscall
+	
+	move $t0, $v0
+WHILE:
+#	Dividi il valore corrente per 2
+	div $t0, $t8
+	
+	mfhi $t1
+	mflo $t0
+#	Controlla se il resto è 1
+	beq $t1, 1, ADDV
+CONDITION:
+#	Controlla se è necessario terminare il ciclo
+	blt $t0, 1, EXIT
+	j WHILE
+ADDV:
+#	Incrementa sommatore
+	add $t9, $t9, 1
+	j CONDITION
+	
+EXIT: 
+#	Stampa l'output e termina il programma
+	la $a0, str_output
 	li $v0, 4
 	syscall 
 	
-	li $v0, 5
-	syscall
-	move $t1, $v0
-	
-	li ITER, 1
-	li DIM, 32
-FOR:
-	and $t2, $t1, 1
-	add ITER, ITER, 1	
-	ble ITER, DIM, FOR
-	
-	la $a0, str_output
-	li $v0, 4
+	move $a0, $t9
+	li $v0, 1
 	syscall
 	
 	li $v0, 10
 	syscall
-
 .data
 str_input: .asciiz "INPUT: "
 str_output: .asciiz "OUTPUT: "
+n: .word 3
